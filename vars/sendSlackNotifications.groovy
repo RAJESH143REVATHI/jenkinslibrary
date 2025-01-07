0f8c143b3a71c53ae17f1f5bdfb9fdf3ce7bbd8f
RAJESH143REVATHI <rajeshthamatam94@gmail.com>
2025-01-07 16:23:04 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,4 +1,4 @@
-def call(String buildStatus = 'STARTED') {
+/*def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
  //This line updated to show the Eclipse with GitHub demo
@@ -24,4 +24,4 @@ def call(String buildStatus = 'STARTED') {
 
   // Calling the slackSend function to Send notifications.
   slackSend (color: colorCode, message: summary)
-}
+}*/
